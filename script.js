3f253fbca1c5f5cc3c5a700d651151405d345d74
Jean Pierre <JeanPCQ@Outlook.com>
2025-09-13 23:48:34 -0400

    New Files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//When Creating a variable, you must follow these rules.
+// Cannot be a reserved keyword
+// Should be meaningful
+// cannot start with a number (1name)
+// Cannot contain a space of hyphen (-)
+// Are case-sensitive
+
+//function let - gives the opperturnity to change the variable
+
+let intrestRate = 0.3;
+intrestRate = 1;
+console.log(intrestRate);
