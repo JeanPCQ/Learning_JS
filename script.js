03137ef8ddb3f55d9b4d5808950cccc89598a18f
Jean Pierre <JeanPCQ@Outlook.com>
2025-09-21 19:46:19 -0400

    Learning something new

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
-let person = {
-    name: 'Jean', age: 29
-};
+const output = document.getElementById("output");
 
-console.log(person);
+//Check two numbers and return true if one of the number is 100 or if the sum of the two is 100
+function checknumbers(){
+
+    const firstNumber = Number(document.getElementById("firstNumber").value);
+    const secondNumber= Number(document.getElementById("secondNumber").value);
+
+    if (firstNumber === 100 || secondNumber === 100){
+        output.textContent = "TRUE";
+    } else if (firstNumber + secondNumber === 100) {
+        output.textContent = "TRUE";
+    } else {
+        output.textContent = "FALSE";
+    }
+}
